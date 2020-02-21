f4aa80286277020da96229ddaabbd858c716c838
Steven Walton <swalton2@cs.uoregon.edu>
2020-02-21 13:52:59 -0800

    Initialize Project 7

---
+++ Project7/proj7.cxx
@@ -0,0 +1,40 @@
+
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkDataSetReader.h>
+#include <vtkDataSetMapper.h>
+#include <vtkActor.h>
+#include <vtkLookupTable.h>
+
+
+int main(int argc, char *argv[])
+{
+   vtkDataSetReader *reader = vtkDataSetReader::New();
+   reader->SetFileName("noise.vtk");
+   reader->Update();
+
+   vtkDataSetMapper *mapper = vtkDataSetMapper::New();
+   mapper->SetInputData(reader->GetOutput());
+   
+   vtkLookupTable *lut = vtkLookupTable::New();
+   mapper->SetLookupTable(lut);
+   mapper->SetScalarRange(1,6);
+   lut->Build();
+
+   vtkActor *actor = vtkActor::New();
+   actor->SetMapper(mapper);
+
+   vtkRenderer *ren = vtkRenderer::New();
+   ren->AddActor(actor);
+
+   vtkRenderWindow *renwin = vtkRenderWindow::New();
+   renwin->AddRenderer(ren);
+
+   vtkRenderWindowInteractor *iren = vtkRenderWindowInteractor::New();
+   iren->SetRenderWindow(renwin);
+   renwin->Render();
+   iren->Start();
+}
+
+
